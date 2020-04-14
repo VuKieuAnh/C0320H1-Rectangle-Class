0dac4c5eaef941bce9e8cbce01a029d8157e0e5b
VuKieuAnh <vukieuanh.hnue@gmail.com>
2020-04-14 13:37:14 +0700

    creat hcn1

--- js.js
+++ js.js
@@ -11,7 +11,27 @@ function Rectangle(w, height) {
     this.getPerimeter = function () {
         return (this.width + this.height)*2;
     }
+    this.drawCanvas = function () {
+        let elementCanvas = document.getElementById("myCanvas");
+        let context = elementCanvas.getContext("2d");
+        context.beginPath();
+        context.fillStyle = "red";
+        context.fillRect(0, 0, this.width, this.height);
+        context.stroke();
+    }
+
+    this.setWidthAndHeight = function (newWidth, newHeight) {
+        this.height = newHeight;
+        this.width =  newWidth;
+    }
 }
 
 let hinhChuNhat1 = new Rectangle(12,20);
 //sử dụng phương thức của đối tượng
+//tên của đối tượng + . + tên của phương thức
+alert("Dien tich hnc1 la "+hinhChuNhat1.getArea());
+
+hinhChuNhat1.drawCanvas();
+
+hinhChuNhat1.setWidthAndHeight(120, 200);
+
